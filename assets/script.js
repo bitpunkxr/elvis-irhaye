82e63775d01a3268fe5a70d5e57e8a6fbbbcd4d8
irhayeelvis <tony69elvis@gmail.com>
2025-11-20 12:19:16 +0100

    fixed mobile UI optimization: 100%

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 // Hamburger toggle
-    const ham = document.querySelector('.ham-menu');
-    const menu = document.querySelector('.off-screen-menu');
-    ham.addEventListener('click', () => {
-      ham.classList.toggle('active');
-      menu.classList.toggle('active');
-    });
-
-    document.querySelectorAll('.off-screen-menu a').forEach(link => {
-      link.addEventListener('click', () => {
-        ham.classList.remove('active');
-        menu.classList.remove('active');
-      });
-    });
+const ham = document.querySelector('.ham-menu');
+const menu = document.querySelector('.off-screen-menu');
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+document.querySelectorAll('.off-screen-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    ham.classList.remove('active');
+    menu.classList.remove('active');
+  });
+});
